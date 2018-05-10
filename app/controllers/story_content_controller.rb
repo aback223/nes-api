477cdab5bf926151773cc83fe29ecc41746bffd5
aback1227 <aback1227@gmail.com>
2018-05-09 21:31:59 -0400

    strong params for content and define a create action

--- app/controllers/story_content_controller.rb
+++ app/controllers/story_content_controller.rb
@@ -3,4 +3,19 @@ class StoryContentController < ApplicationController
     @contents = StoryContent.all
     render json: @contents
   end
+
+  def create
+    @content = StoryContent.new(content_params)
+    if @content.save
+      render json: @content
+    else
+      render json: @content.errors
+    end
+  end
+
+  private
+
+  def content_params
+    params.require(:story_content).permit(:created_at, :content)
+  end
 end

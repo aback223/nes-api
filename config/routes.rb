ef80dcafccd34672a761acceef7113bb82e42aac
aback1227 <aback1227@gmail.com>
2018-05-11 22:48:13 -0400

    change routing urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :story_content
+  get '/story_contents', to: 'story_content#index'
+  post '/story_contents', to: 'story_content#create'
+  get '/story_contents/:id', to: 'story_content#show', as: 'story_content'
+  patch '/story_contents/:id', to: 'story_content#update'
+  delete '/story_contents/:id', to: 'story_content#destroy' 
 end

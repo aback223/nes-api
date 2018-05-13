420b11ada7b5c0ef58fba8d8959b77f170c7957b
aback1227 <aback1227@gmail.com>
2018-05-13 11:39:54 -0400

    cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:4000', 'https://neverendingstory.herokuapp.com/']
+    origins ['localhost:4000', 'neverendingstory.herokuapp.com']
 
     resource '*',
       headers: :any,

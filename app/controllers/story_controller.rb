491bf9e351827bd908d3ca3887e343d1a9713700
aback1227 <aback1227@gmail.com>
2018-05-18 21:04:38 -0400

    define a create action and story_params method in storycontroller

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -3,4 +3,19 @@ class StoryController < ApplicationController
     @stories = Story.all
     render json: @stories
   end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      render json: @story
+    else
+      render json: @story.errors
+    end
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:title, :content, :created_at)
+  end
 end

6c9bc57eaca2fd8bdea1d3c639940d215e183a94
aback1227 <aback1227@gmail.com>
2018-05-18 20:57:43 -0400

    add routes for story

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/story_contents/:id', to: 'story_content#show', as: 'story_content'
   patch '/story_contents/:id', to: 'story_content#update'
   delete '/story_contents/:id', to: 'story_content#destroy' 
+
+  resources :story
 end

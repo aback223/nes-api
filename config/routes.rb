9c94d8ae8ee3f1758e2a336e4367ad18a78723af
aback1227 <aback1227@gmail.com>
2018-05-18 22:39:57 -0400

    remove routes for content, all routes for story except destroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   patch '/story_contents/:id', to: 'story_content#update'
   delete '/story_contents/:id', to: 'story_content#destroy' 
 
-  resources :story
-  resources :content, only: [:create, :index]
+  resources :story, only: [:index, :create, :show, :update]
 end

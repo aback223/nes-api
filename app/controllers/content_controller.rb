c8ba71a71acdbd73aafa74f7c3a97bd0a9d95ea9
aback1227 <aback1227@gmail.com>
2018-07-31 11:52:08 -0400

    no change

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,11 +1,11 @@
 class ContentController < ApplicationController
   def index
-    @contents = Story.find(params[:id]).contents
+    @contents = Story.find(params[:story_id]).contents
     render json: @contents
   end
 
   def create
-    story = Story.find(params[:id])
+    story = Story.find(params[:story_id])
     @content = story.contents.new(content: params[:content])
     if @content.save
       render json: @content

bf7c469d5fcc618f78ace4d6be4c33940d6e01ce
aback1227 <aback1227@gmail.com>
2018-07-31 11:50:29 -0400

    create a users table and User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180519022543) do
+ActiveRecord::Schema.define(version: 20180731050846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20180519022543) do
   create_table "story_contents", force: :cascade do |t|
     t.datetime "created_at"
     t.string   "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
   end
 
 end
